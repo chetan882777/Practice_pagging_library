a232f7227b984b3b3bfeebbe4b5aa81ae9183bfe
chetan882777 <chetan882777@gmail.com>
2019-05-10 22:17:55 +0530

    Add Retrofit api interface

---
+++ app/src/main/java/com/chetan/practicepagginglibrary/RestApi.java
@@ -0,0 +1,20 @@
+package com.chetan.practicepagginglibrary;
+
+import retrofit2.http.GET;
+import retrofit2.http.Query;
+
+public interface RestApi {
+
+
+    /*
+     * We would be using the below url:
+     * https://newsapi.org/v2/everything?q=movies&apiKey=079dac74a5f94ebdb990ecf61c8854b7&pageSize=20&page=2
+     * The url has four query parameters.
+     * We would be changing the pageSize and the page
+     */
+    @GET("/v2/everything")
+    Call<Feed> fetchFeed(@Query("q") String q,
+                         @Query("apiKey") String apiKey,
+                         @Query("page") long page,
+                         @Query("pageSize") int pageSize);
+}
